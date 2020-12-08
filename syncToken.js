068006f2fb75870e00db9fe70de59a7279f0685d
史祥宇 <sxy@dataqin.com>
2020-12-08 15:31:47 +0800

    add synctoken

---
+++ syncToken.js
@@ -0,0 +1,26 @@
+
+const mysql = require('mysql2');
+const connnect = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'multi_chain_wallet'
+})
+
+const tokens = require('./eth_token')
+function spider() { 
+
+    tokens.forEach((token) =>{ 
+        const {contract,symbol,icon,name,price,sort } = token;
+        connnect.execute(`replace into eth_token(contract,symbol,icon,name,sort,price) 
+        values('${contract}','${symbol}','${icon}','${name}',${sort},${price})`, function (err,res) { 
+                if (!err) {
+                    console.log('success',sort)
+                } else { 
+                    console.log(err)
+                }
+        })
+        
+    })
+}
+spider()
