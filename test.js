65360718f5dcc3b8f7c9a19af53824177c238ed9
史祥宇 <sxy@dataqin.com>
2020-12-14 18:44:30 +0800

    --

--- test.js
+++ test.js
@@ -2,15 +2,4 @@ const Web3 = require('web3');
 const web3 = new Web3(
   new Web3.providers.HttpProvider('http://8.210.111.37:8545'),
 );
-console.log(
-  web3.eth.abi.decodeParameters(
-    [{
-      type: 'address',
-      name: 'to'
-  },{
-      type: 'uint256',
-      name: 'value'
-  }],
-    '0x00000000000000000000000094c98c8e336bae512bb648a6b560b50711b11e1f00000000000000000000000000000000000000000000000000000000000186a0',
-  ),
-);
+console.log(web3.eth.accounts.privateKeyToAccount('0x5eee27fc11dcf941e19e5f79bdf55b9e53c7a8a5953ff589efb90e87591625d1'))
