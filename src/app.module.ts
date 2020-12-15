64bef29795a4789022ce09b2ef4c5a5f010dfa5f
史祥宇 <sxy@dataqin.com>
2020-12-15 11:01:46 +0800

    crom

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { CoreModule } from './core';
 import { SipcModule } from './shared';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
       exclude: ['/api*'],
