589fd36ee87a23d04678956c8d602706ca9cd3c5
史祥宇 <sxy@dataqin.com>
2020-12-16 13:51:38 +0800

    -

--- src/shared/utils/time.ts
+++ src/shared/utils/time.ts
@@ -3,6 +3,19 @@ export async function sleep(millisecond: number) {
   return new Promise((resolve) => setTimeout(resolve, millisecond));
 }
 
-function getCurrentWeek() {
-  return moment().unix() / 604800;
+export function getWeekYear(createTable: boolean) {
+  let week: string | number = moment(moment().format('YYYY-MM-DD')).isoWeek();
+
+  let year = moment(moment().format('YYYY-MM-DD')).isoWeekYear();
+
+  // 创建下一周的表
+  createTable && week++;
+  if (week > 53) {
+    week = 1;
+    year++;
+  }
+  if (week < 10) {
+    week = '0' + week;
+  }
+  return year + '' + week;
 }
