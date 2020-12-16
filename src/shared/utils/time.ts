a49a490f9d757053001d8fd3a847933c34a30eb2
史祥宇 <sxy@dataqin.com>
2020-12-16 10:52:34 +0800

    --

--- src/shared/utils/time.ts
+++ src/shared/utils/time.ts
@@ -1,3 +1,8 @@
-export async function sleep(millisecond:number) {
-    return new Promise((resolve) => setTimeout(resolve, millisecond));
+import * as moment from 'moment';
+export async function sleep(millisecond: number) {
+  return new Promise((resolve) => setTimeout(resolve, millisecond));
+}
+
+function getCurrentWeek() {
+  return moment().unix() / 604800;
 }
