f9eeca1efebb17e647d75d1773f48e3040a7a357
史祥宇 <sxy@dataqin.com>
2020-12-18 12:20:42 +0800

    upgrade https

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './core/interceptor/transform.interceptor';
-
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as http from 'http';
+import * as https from 'https';
+import * as express from 'express';
+import * as fs from 'fs';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  };
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   app.setGlobalPrefix('api/v1');
   // 全局使用管道
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3001);
+  await app.init();
+
+  http.createServer(server).listen(3000);
+  https.createServer(httpsOptions, server).listen(3001);
 }
 bootstrap();
