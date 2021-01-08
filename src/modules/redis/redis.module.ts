4546eeb49eb90e40269d02b1131435845b810d0a
史祥宇 <sxy@dataqin.com>
2021-01-08 15:25:27 +0800

    --

--- src/modules/redis/redis.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { redisProviders } from './redis.provider';
-import { ConfigModule } from '../../core';
-@Module({
-  imports: [ConfigModule],
-  providers: [...redisProviders],
-  exports: [...redisProviders],
-})
-export class RedisModule {}
